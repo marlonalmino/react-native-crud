bcf59c691b445bce9beca121c19856c541ecde88
Marlon Almino <alminomarlon@gmail.com>
2022-12-23 18:11:56 -0300

    Lista de Usuários #01

--- src/views/UserList.js
+++ src/views/UserList.js
@@ -1,8 +1,32 @@
 import React from 'react'
-import { Text } from 'react-native'
+import { FlatList, View } from 'react-native'
+import { Avatar, ListItem } from '@rneui/themed'
+import users from '../data/users'
 
 export default props => {
+
+  function getUserItem({ item: user }) {
+    return (
+      <ListItem
+        onPress={() => props.navigation.navigate('UserForm')}
+        bottomDivider
+        topDivider>
+        <Avatar title={user.name} source={{ uri: user.avatarUrl }} rounded />
+        <ListItem.Content>
+          <ListItem.Title>{user.name}</ListItem.Title>
+          <ListItem.Subtitle>{user.email}</ListItem.Subtitle>
+        </ListItem.Content>
+      </ListItem>
+    )
+  }
+
   return (
-    <Text>UserList</Text>
+    <View>
+      <FlatList
+        keyExtractor={user => user.id.toString()}
+        data={users}
+        renderItem={getUserItem}
+      />
+    </View>
   )
 }
