0f45696605aee4fed07eef63d57d9874709c16c6
Marlon Almino <alminomarlon@gmail.com>
2022-12-23 16:30:42 -0300

    Criando Arquivo de Usuários

---
+++ src/data/users.js
@@ -0,0 +1,26 @@
+export default [
+  {
+    id: 1,
+    name: 'João Silva',
+    email: 'josil@empmail.com',
+    avatarUrl: 'https://cdn.pixabay.com/photo/2016/03/31/19/58/avatar-1295430_960_720.png'
+  },
+  {
+    id: 2,
+    name: 'Mariana Aguiar',
+    email: 'riamasil@empmail.com',
+    avatarUrl: 'https://cdn.pixabay.com/photo/2017/04/30/11/42/hijab-2272708_960_720.png'
+  },
+  {
+    id: 3,
+    name: 'Julia Fragoso',
+    email: 'juliafral@empmail.com',
+    avatarUrl: 'https://cdn.pixabay.com/photo/2014/04/02/14/10/female-306407_960_720.png'
+  },
+  {
+    id: 4,
+    name: 'Rafael Monteiro',
+    email: 'rafamontee@empmail.com',
+    avatarUrl: 'https://cdn.pixabay.com/photo/2013/07/13/10/07/man-156584_960_720.png'
+  },
+]
