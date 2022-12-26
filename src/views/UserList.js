84ed0deaa649e72d620e6b5b548c73e98ff80a0f
Marlon Almino <alminomarlon@gmail.com>
2022-12-26 11:05:06 -0300

    Lista de Usuários #02

--- src/views/UserList.js
+++ src/views/UserList.js
@@ -1,9 +1,23 @@
 import React from 'react'
-import { FlatList, View } from 'react-native'
-import { Avatar, ListItem } from '@rneui/themed'
+import { Alert, FlatList, View } from 'react-native'
+import { Avatar, ListItem, Button, Icon } from '@rneui/themed'
 import users from '../data/users'
 
 export default props => {
+
+  function confirmUserDeletion(user) {
+    Alert.alert('Excluir Usuário', 'Deseja excluir o usuário?', [
+      {
+        text: 'Sim',
+        onPress() {
+          console.warn('delete')
+        }
+      },
+      {
+        text: 'Não'
+      },
+    ])
+  }
 
   function getUserItem({ item: user }) {
     return (
@@ -16,6 +30,19 @@ export default props => {
           <ListItem.Title>{user.name}</ListItem.Title>
           <ListItem.Subtitle>{user.email}</ListItem.Subtitle>
         </ListItem.Content>
+
+        <Button
+          onPress={() => {
+            props.navigation.navigate('UserForm', user);
+          }}
+          type="clear"
+          icon={<Icon name="edit" size={25} color="orange" />}
+        />
+        <Button
+          onPress={() => { confirmUserDeletion(user) }}
+          type="clear"
+          icon={<Icon name="delete" size={25} color="red" />}
+        />
       </ListItem>
     )
   }
