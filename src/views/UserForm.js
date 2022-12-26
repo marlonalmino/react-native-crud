1ad75b08e2661137b3adf18fb39718550eb7f3fe
Marlon Almino <alminomarlon@gmail.com>
2022-12-26 15:00:11 -0300

    Formulário de Usuário

--- src/views/UserForm.js
+++ src/views/UserForm.js
@@ -1,9 +1,57 @@
 import React, { useState } from 'react'
-import { Text } from 'react-native'
+import { Text, TextInput, View, StyleSheet, Button } from 'react-native'
 
-export default ({ navigation, route }) => {
+export default ({ route, navigation }) => {
   const [user, setUser] = useState(route.params ? route.params : {})
   return (
-    <Text>{user.id}</Text>
+    <View style={style.form}>
+      <Text style={style.label}>Nome</Text>
+      <TextInput
+        style={style.input}
+        onChangeText={name => setUser({ ...user, name })}
+        placeholder='Informe o Nome'
+        value={user.name}
+      />
+
+      <Text style={style.label}>E-mail</Text>
+      <TextInput
+        style={style.input}
+        onChangeText={email => setUser({ ...user, email })}
+        placeholder='Informe o E-mail'
+        value={user.email}
+      />
+
+      <Text style={style.label}>URL do Avatar</Text>
+      <TextInput
+        style={style.input}
+        onChangeText={avatarUrl => setUser({ ...user, avatarUrl })}
+        placeholder='Informe a URL do Avatar'
+        value={user.avatarUrl}
+      />
+
+      <Button
+        title='Salvar'
+        onPress={() => {
+          navigation.goBack()
+        }}
+      />
+    </View>
   )
 }
+
+const style = StyleSheet.create({
+  form: {
+    padding: 12,
+  },
+  input: {
+    height: 40,
+    borderColor: 'gray',
+    borderWidth: 1,
+    borderRadius: 15,
+    marginBottom: 15,
+    paddingLeft: 15,
+  },
+  label: {
+    marginLeft: 15
+  },
+})
