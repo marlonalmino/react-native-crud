6a282915b107d312f8180fe8e5600426af57fe8b
Marlon Almino <alminomarlon@gmail.com>
2022-12-27 13:02:20 -0300

    Context API

--- src/App.js
+++ src/App.js
@@ -4,40 +4,43 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack'
 import UserList from './views/UserList'
 import UserForm from './views/UserForm'
 import { Button, Icon } from '@rneui/themed'
+import { UsersProvider } from './context/UserContext'
 
 const Stack = createNativeStackNavigator()
 
 export default props => {
   return (
-    <NavigationContainer>
-      <Stack.Navigator
-        initialRouteName='UserList'
-        screenOptions={screenOptions} >
-        <Stack.Screen
-          name='UserList'
-          component={UserList}
-          options={({ navigation }) => {
-            return {
-              title: 'Lista de Usuários',
-              headerRight: () => (
-                <Button
-                  onPress={() => navigation.navigate('UserForm')}
-                  type='clear'
-                  icon={<Icon name='add' size={25} color='white' />}
-                />
-              )
-            }
-          }}
-        />
-        <Stack.Screen
-          name='UserForm'
-          component={UserForm}
-          options={{
-            title: 'Formulário de Usuários'
-          }}
-        />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <UsersProvider>
+      <NavigationContainer>
+        <Stack.Navigator
+          initialRouteName='UserList'
+          screenOptions={screenOptions} >
+          <Stack.Screen
+            name='UserList'
+            component={UserList}
+            options={({ navigation }) => {
+              return {
+                title: 'Lista de Usuários',
+                headerRight: () => (
+                  <Button
+                    onPress={() => navigation.navigate('UserForm')}
+                    type='clear'
+                    icon={<Icon name='add' size={25} color='white' />}
+                  />
+                )
+              }
+            }}
+          />
+          <Stack.Screen
+            name='UserForm'
+            component={UserForm}
+            options={{
+              title: 'Formulário de Usuários'
+            }}
+          />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </UsersProvider>
   )
 }
 
--- src/views/UserList.js
+++ src/views/UserList.js
@@ -1,9 +1,11 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import { Alert, FlatList, View } from 'react-native'
 import { Avatar, ListItem, Button, Icon } from '@rneui/themed'
-import users from '../data/users'
+import UsersContext from '../context/UserContext'
 
 export default props => {
+
+  const { state } = useContext(UsersContext)
 
   function confirmUserDeletion(user) {
     Alert.alert('Excluir Usuário', 'Deseja excluir o usuário?', [
@@ -50,7 +52,7 @@ export default props => {
     <View>
       <FlatList
         keyExtractor={user => user.id.toString()}
-        data={users}
+        data={state.users}
         renderItem={getUserItem}
       />
     </View>
