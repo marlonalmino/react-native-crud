97eb9518f097a509a1e4c8f59e446e238d3aeef0
Marlon Almino <alminomarlon@gmail.com>
2022-12-28 13:23:39 -0300

    useReducer #03

--- src/context/UserContext.js
+++ src/context/UserContext.js
@@ -5,13 +5,30 @@ const initialState = { users }
 const UsersContext = createContext({})
 
 const actions = {
+  createUser(state, action) {
+    const user = action.payload
+    user.id = Math.random()
+    return {
+      ...state,
+      users: [...state.users, user]
+    }
+  },
+
+  updateUser(state, action) {
+    const updated = action.payload
+    return {
+      ...state,
+      users: state.users.map(u => u.id === updated.id ? updated : u)
+    }
+  },
+
   deleteUser(state, action) {
     const user = action.payload
     return {
-      // ...state,
+      ...state,
       users: state.users.filter(u => u.id !== user.id)
     }
-  }
+  },
 }
 
 export const UsersProvider = props => {
--- src/views/UserForm.js
+++ src/views/UserForm.js
@@ -1,8 +1,11 @@
-import React, { useState } from 'react'
-import { Text, TextInput, View, StyleSheet, Button } from 'react-native'
+import React, { useContext, useState } from 'react'
+import { Text, TextInput, View, StyleSheet, Button, ActionSheetIOS } from 'react-native'
+import UsersContext from '../context/UserContext'
 
 export default ({ route, navigation }) => {
   const [user, setUser] = useState(route.params ? route.params : {})
+  const { dispatch } = useContext(UsersContext)
+
   return (
     <View style={style.form}>
       <Text style={style.label}>Nome</Text>
@@ -32,6 +35,10 @@ export default ({ route, navigation }) => {
       <Button
         title='Salvar'
         onPress={() => {
+          dispatch({
+            type: user.id ? 'updateUser' : 'createUser',
+            payload: user,
+          })
           navigation.goBack()
         }}
       />
